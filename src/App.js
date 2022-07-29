3f38b3be393c654a3b12d4d98c09216d4fe3d4c5
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-07-29 05:26:01 +0300

    Renders the navbar links in the App component and gives them routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react"
 import Home from "./Home";
+import About from "./About";
+import TrendList from "./TrendList";
+import Contact from "./Contact";
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 
@@ -7,6 +10,9 @@ function App() {
   return (
     <div>
        <Routes >
+       <Route exact path="/about" element={<About/>} />
+         <Route exact path="/trendlist" element={<TrendList/>} />
+         <Route exact path="/contact" element={<Contact/>} />
          <Route exact path="/" element={<Home/>} />
       </Routes>
     </div>
