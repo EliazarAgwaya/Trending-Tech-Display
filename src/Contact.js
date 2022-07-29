64c75fc8f89518296e8f3365e74df27ea00f0b9b
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-07-29 05:31:37 +0300

    Adds the contact component's contents

---
+++ src/Contact.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function Contact () {
+    return (
+        <div className="container">
+           <div className="contact">
+            <h2> CONTACT US!</h2>
+                <h5>Any inqury?</h5>
+                <p>Send us a message</p>
+                <form className="form-elements">
+                    <input type="text"  placeholder="Your name"/><br/><br/>
+                    <input name="email" type="text"  placeholder="Your Email"/><br/><br/>
+                    <textarea name=""  cols="30" rows="10" placeholder="Massage...."/><br/><br/>
+                    <span><input type="checkbox"/>  I am not a robot </span> <br/><br/>
+                    <button id="submit">SEND</button>
+                </form>
+           </div>
+        </div>
+    )
+
+}
+export default Contact
