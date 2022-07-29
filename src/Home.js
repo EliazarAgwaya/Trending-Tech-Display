4b0785e452b778aac918bf5c891c367791c958cd
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-07-29 06:43:36 +0300

    Renders the Slider component in the home component

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,14 @@
 import React from "react"
 import NavBar from "./NavBar"
+import Slider from "./Slider"
 
 function Home () {
 
+    const container = {
+        width: "500px",
+        height: "280px",
+        margin: "0 auto",
+    }
     return (
         <div className="home-header">
             <div className="home-nav">
@@ -13,6 +19,14 @@ function Home () {
             <div className="home">
                 <h2>Be upto date with the current trends in technology</h2>
                 <h4>The field of technology is one which keeps changing every minute. There are very many trends in technology today which everyone needs to know</h4>
+                <div className="centre">
+                    <div className="space">
+                        <div style={container}>
+                            <Slider/>
+                        </div>
+                    </div>
+                
+                </div>
             </div>
             
         </div>
